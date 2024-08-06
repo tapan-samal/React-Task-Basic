041182fd2d7191b40aa4b6b75889b4266f926f1b
Tapan Samal <145225882+tapan-samal@users.noreply.github.com>
2024-08-06 15:23:56 +0530

    Update DisplayFormData.js

--- DisplayFormData.js
+++ DisplayFormData.js
@@ -1,35 +1,55 @@
 import React, { useState } from "react";
 
 const DisplayFormData = () => {
-  const [formData, setFormData] = useState({ userName: "", email: "" });
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [formData, setFormData] = useState({ fullname: "", email: "" });
+  const [isSubmitted, setIsSubmitted] = useState(null);
 
-  const handleInputChange = (e) => {
+  const handleInputData = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
-
-  const handleSubmitClick = (e) => {
+  const handleSubmitBtn = (e) => {
     e.preventDefault();
-    setIsSubmitted(true);
-    // setFormData({ userName: "", email: "" });
+    setIsSubmitted(formData);
+    setFormData({ fullname: "", email: "" });
   };
 
   return (
     <>
-      <h2>Input Form Data</h2>
-      <form onSubmit={handleSubmitClick}>
-        <label>User Name: <input type="text" name="userName" value={formData.userName} onChange={handleInputChange} /></label>{" "}
-        <label>Email: <input type="email" name="email" value={formData.email} onChange={handleInputChange} /></label>
+      <h2>Form Data</h2>
+      <form onSubmit={handleSubmitBtn}>
+        <div>
+          <label>Full Name: </label> <br />
+          <input
+            type="text"
+            placeholder="Full Name..."
+            name="fullname"
+            value={formData.fullname}
+            onChange={handleInputData}
+            required
+          />
+        </div>{" "}
+        <br />
+        <div>
+          <label>Email: </label> <br />
+          <input
+            type="email"
+            placeholder="Email..."
+            name="email"
+            value={formData.email}
+            onChange={handleInputData}
+            required
+          />
+        </div>{" "}
         <br />
         <button type="submit">Submit</button>
       </form>
       {isSubmitted && (
-        <>
-          <h3>Display Form Data:</h3>
-          <p>User Name: {formData.userName}</p>
-          <p>Email: {formData.email}</p>
-        </>
+        <div>
+          <h2>Form Data:</h2>
+          <h4>{isSubmitted.fullname}</h4>
+          <h4>{isSubmitted.email}</h4>
+        </div>
       )}
     </>
   );
